6cd8037fcad612f6cdc6d8597c2222716eebdd0d
Chrisjsa <chris.jsa01@gmail.com>
2025-03-25 13:22:58 -0400

    - Remove moving between directories

--- setup.sh
+++ setup.sh
@@ -7,31 +7,27 @@ echo "Hardhat dependencies installed."
 
 # --- 2. Create API Virtual Environment ---
 echo "Creating API virtual environment..."
-cd api
-python3 -m venv venv
-source venv/bin/activate
+python3 -m venv api/venv
+source api/venv/bin/activate
 echo "API virtual environment created."
 
 # --- 3. Install API Requirements ---
 echo "Installing API requirements..."
-pip install -r requirements.txt
+pip install -r api/requirements.txt
 echo "API requirements installed."
 
 # --- 4. Deploy Contracts with Hardhat ---
 echo "Deploying contracts with Hardhat..."
-cd ..
 npm run deploy
 echo "Contracts deployed."
 
 # --- 5. Deploy API with Gunicorn ---
 echo "Deploying API with Gunicorn..."
-cd api
 # run api through gunicorn
-venv/bin/gunicorn --bind 0.0.0.0:8000 app:app
+api/venv/bin/gunicorn --bind 0.0.0.0:8000 app:app
 echo "API deployed."
 
 # --- 6. Deploy Frontend ---
-cd ..
 echo "Deploying frontend..."
 cd frontend_basic
 npm run build && npm run serve
